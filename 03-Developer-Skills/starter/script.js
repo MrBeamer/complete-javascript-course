d97607360caaa740f60a23683d55a73f91ea8b93
michael beamer <beamermichael@gmail.com>
2021-01-12 16:35:56 +0100

    Finished Chapter 3

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,20 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+/*
+///////////////////////////////////////
+// Coding Challenge #1
+
+const arr1 = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `...${arr[i]}oC in ${i + 1} days `;
+  }
+  console.log(str);
+}
+printForecast(arr1);
+
+/*
