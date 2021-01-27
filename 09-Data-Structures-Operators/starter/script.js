74102f5554a6cbf023469dc6d0cb32809ffc4904
michael beamer <beamermichael@gmail.com>
2021-01-27 18:56:13 +0100

    Chapter 9 lessons 114

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -173,6 +173,121 @@ const restaurant = {
 ///////////////////////////////////////
 // CODING CHALLENGE #1:
 
+// const game = {
+//   team1: 'Bayern munich',
+//   team2: 'Borrussia Dortmund',
+//   player: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+//   printWinner() {},
+
+//   printGoals(...players) {
+//     console.log(players);
+//     console.log(
+//       `Scorer: ${players} ${players.length} goals have fallen that game.`
+//     );
+//   },
+// };
+
+// const [players1, players2] = [...game.player];
+// console.log(players1);
+// console.log(players2);
+
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk);
+// console.log(fieldPlayers);
+
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1);
+// console.log(draw);
+// console.log(team2);
+
+// game.printGoals(...game.scored);
+// game.printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
+// const fullMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const dish of fullMenu) console.log(dish);
+// for (const dish of fullMenu.entries()) {
+//   let [number, food] = dish;
+//   console.log(`${number + 1}: ${food}`);
+// }
+
+// console.log(restaurant.openingHours.mon.open);
+// console.log(restaurant.openingHours.mon?.open ?? 'This day dont exist.');
+
+// const store = {
+//   goods: {
+//     consoles: {
+//       xbox: 400,
+//     },
+//     phones: {
+//       samsung: 500,
+//       apple: 300,
+//     },
+//   },
+// };
+
+// console.log(store.goods.consoles?.ps5 ?? 'Ps5 is still sold out');
+
+// const keys = Object.keys(restaurant.openingHours);
+// console.log(keys);
+
+// const values = Object.values(restaurant.openingHours);
+// console.log(values);
+
+// const entries = Object.entries(restaurant.openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}.`);
+// }
+
+///////////////////////////////////////
+// CODING CHALLENGE #2:
+
 const game = {
   team1: 'Bayern munich',
   team2: 'Borrussia Dortmund',
@@ -222,27 +337,36 @@ const game = {
   },
 };
 
-const [players1, players2] = [...game.player];
-console.log(players1);
-console.log(players2);
+// // 1
+// for (const [goal, player] of game.scored.entries()) {
+//   console.log(`Goal ${goal + 1}:${player}`);
+// }
 
-const [gk, ...fieldPlayers] = players1;
-console.log(gk);
-console.log(fieldPlayers);
+// 2
+// const oddValues = Object.values(game.odds);
 
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// let odds = 0;
 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
+// for (const value of oddValues) {
+//   odds += value;
+// }
 
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1);
-console.log(draw);
-console.log(team2);
+// const avgOdd = odds / oddValues.length;
+// console.log(avgOdd);
 
-game.printGoals(...game.scored);
-game.printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// 3
+// const entriesGame = Object.entries(game.odds);
+// console.log(entriesGame);
 
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
+// for (const [team, odd] of entriesGame) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr}: ${odd}`);
+// }
+
+// bonus
+
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
