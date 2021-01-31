7d25b1b1ab26c35549606b7026014dc71a33f3d8
michael beamer <beamermichael@gmail.com>
2021-01-31 15:21:15 +0100

    Finished Chapter 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -179,11 +179,11 @@
 ///////////////////////////////////////
 // Coding Challenge #2
 
-(function () {
-  const header = document.querySelector('h1');
-  header.style.color = 'purple';
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'purple';
 
-  document.querySelector('body').addEventListener('click', function () {
-    header.style.color = 'blue';
-  });
-})();
+//   document.querySelector('body').addEventListener('click', function () {
+//     header.style.color = 'blue';
+//   });
+// })();
