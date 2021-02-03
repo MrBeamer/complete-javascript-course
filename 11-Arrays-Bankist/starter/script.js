cde91e8facd7571eb4bad221487c8f55f3563342
michael beamer <beamermichael@gmail.com>
2021-02-03 18:45:28 +0100

    Chapter 11 finished lesson 164

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-function displayMovement(movements) {
+function displayMovement(movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach((movement, index) => {
+  const mov = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  mov.forEach((movement, index) => {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     const html = `<div class="movements__row">
@@ -78,6 +80,13 @@ function displayMovement(movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 }
+
+let sorted = false;
+btnSort.addEventListener('click', function (event) {
+  event.preventDefault();
+  displayMovement(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 
 //calc and display balance
 
@@ -180,6 +189,40 @@ btnTransfer.addEventListener('click', function (event) {
     receiverAccount.movements.push(amount);
     updateUi(currentAccount);
     inputTransferTo.value = inputTransferAmount.value = '';
+  }
+});
+
+btnLoan.addEventListener('click', function (event) {
+  event.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUi(currentAccount);
+    inputLoanAmount.value = '';
+  }
+});
+
+//close account
+btnClose.addEventListener('click', function (event) {
+  event.preventDefault();
+  const targetUsername = inputCloseUsername.value;
+  const targetPin = Number(inputClosePin.value);
+  if (
+    // accounts[accIndex].username === targetUsername &&
+    // accounts[accIndex].pin === targetPin
+    currentAccount.username === targetUsername &&
+    currentAccount.pin === targetPin
+  ) {
+    const accIndex = accounts.findIndex(acc => acc.username === targetUsername);
+    accounts.splice(accIndex, 1);
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = `Bye ${
+      currentAccount.owner.split(' ')[0]
+    } comeback soon`;
+    function reset() {
+      labelWelcome.textContent = 'Log in to get started';
+    }
+    setTimeout(reset, 4000);
   }
 });
 
@@ -307,3 +350,56 @@ GOOD LUCK 😀
 //   .map(mov => mov * eurToUsd)
 //   .reduce((acc, mov) => acc + mov, 0);
 // console.log(totalDepoInUsd);
+
+// const accountMov = accounts.flatMap(mov => mov.movements);
+// console.log(accountMov);
+
+// const newArr = accountMov
+//   .flatMap(mov => mov)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(newArr);
+// const test = document.querySelectorAll('.movements__value');
+
+// test.forEach(el => console.log(el));
+// console.log(test);
+
+//// array methods practice
+// 1
+const sumDeposit = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((acc, mov) => acc + mov);
+console.log(sumDeposit);
+
+// 2
+const depositAbove = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov >= 1000).length;
+
+console.log(depositAbove);
+///////////////////////////////////////
+// Coding Challenge #4
+
+// Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+// Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+// Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+// 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+// 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+// 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+// 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+// 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+// 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+// HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+// HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+// TEST DATA:
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] }
+// ];
