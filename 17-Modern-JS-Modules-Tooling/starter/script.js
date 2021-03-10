b830fcb12b10ef05c11e892a13216915ec4315f6
michael beamer <beamermichael@gmail.com>
2021-03-10 13:50:39 +0100

    Finished Chapter 17

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,15 @@
+// Importing module
+import {
+  addToCart,
+  totalPrice as price,
+  totalQuantity,
+} from './shoppingCard.js';
+// console.log('Importing module');
+// addToCart('fish', 2);
+// console.log(price, totalQuantity);
+
+// import * as ShoppingCart from './shoppingCard.js';
+// ShoppingCart.addToCart('bread', 5);
+import cloneDeep from '../../node_modules/lodash-es/cloneDeep.js';
+
+npx parcel 
